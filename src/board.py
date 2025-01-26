1d4afbf5fe1763646f26841611ea2847cd5e2a30
Viktor Pazhev <asusvikp@gmail.com>
2025-01-26 18:22:18 +0200

    Add small board win

--- src/board.py
+++ src/board.py
@@ -2,34 +2,39 @@ import numpy as np
 import numpy.typing as npt
 
 class Board:
+    EMPTY: str = " "
+    FULL: str = "F"
+
     def __init__(self):
-        self.board: npt.NDArray[np.str_] = np.full((3, 3, 3, 3), " ")
+        self.board: npt.NDArray[np.str_] = np.full((3, 3, 3, 3), self.EMPTY)
+        self.big_board: npt.NDArray[np.str_] = np.full((3, 3), self.EMPTY)
 
     def play_turn(self, player: str, bigX: int, bigY: int, smallX: int, smallY: int) -> bool:
-        if self.board[bigY, bigX, smallY, smallX] != " ":
+        current_board: npt.NDArray[np.str_] = self.board[bigY, bigX]
+
+        if self.big_board[bigY, bigX] != self.EMPTY or current_board[smallY, smallX] != self.EMPTY:
             return False
         
-        self.board[bigY, bigX, smallY, smallX] = player
+        current_board[smallY, smallX] = player
+
+        if Board.check_board_win(current_board, player):
+            self.big_board[bigY, bigX] = player
+        elif (current_board != self.EMPTY).all():
+            self.big_board[bigY, bigX] = self.FULL
+
         return True
     
+    def check_small_board_valid(self, bigX: int, bigY: int) -> bool:
+        return self.big_board[bigY, bigX] == self.EMPTY
+    
     def check_small_win(self, player: str, bigX: int, bigY: int) -> bool:
-        small_board: npt.NDArray[np.str_] = self.board[bigX, bigY]
-
-        for i in range(3):
-            if (small_board[i] == player).all() \
-                or (small_board[:, i] == player).all() \
-                or (np.diag(small_board) == player).all() \
-                or (np.diag(np.fliplr(small_board)) == player).all():
-
-                return True
-
-        return False
+        return self.big_board[bigY, bigX] == player
     
     def check_big_win(self, player: str) -> bool:
-        return False
+        return Board.check_board_win(self.big_board, player)
     
     def is_full(self) -> bool:
-        return False
+        return all(not self.check_small_board_valid(bigX, bigY) for bigX in range(3) for bigY in range(3))
     
     def print(self) -> None:
         print("-" * 41)
@@ -44,3 +49,15 @@ class Board:
                 print()
 
             print("-" * 41)
+
+    @staticmethod
+    def check_board_win(board: npt.NDArray[np.str_], player: str) -> bool:
+        for i in range(3):
+            if (board[i] == player).all() \
+                or (board[:, i] == player).all() \
+                or (np.diag(board) == player).all() \
+                or (np.diag(np.fliplr(board)) == player).all():
+
+                return True
+
+        return False
--- src/game.py
+++ src/game.py
@@ -4,21 +4,22 @@ class Game:
     def __init__(self):
         self.player1: str = "O"
         self.player2: str = "X"
-        self.current_player: str = self.player1
+
         self.board: Board = Board()
+        self.next_x: int = -1
+        self.next_y: int = -1
+
+        self.current_player: str = self.player1
         self.winner: str | None = None
         self.play()
 
-    def play(self):
+    def play(self) -> None:
         while not self.board.is_full() and not self.winner:
             self.print()
             self.play_turn()
-            #self.winner = self.board.get_winner()
+            self.switch_player()
 
-            if self.current_player == self.player1:
-                self.current_player = self.player2
-            else:
-                self.current_player = self.player1
+        self.print()
 
         if not self.winner:
             print("It's a tie!")
@@ -31,25 +32,43 @@ class Game:
         else:
             self.current_player = self.player1
 
-    def play_turn(self) -> None:
-        bigX: int = int(input("Enter big X: "))
-        bigY: int = int(input("Enter big Y: "))
+    def get_turn_coordinates(self) -> tuple[int, int, int, int]:
+        if self.next_x == -1:
+            print("You can play anywhere!")
+            bigX: int = int(input("Enter big X: "))
+            bigY: int = int(input("Enter big Y: "))
+        else:
+            print(f"You must play in the ({self.next_x}, {self.next_y}) board!")
+            bigX: int = self.next_x
+            bigY: int = self.next_y
+
         smallX: int = int(input("Enter small X: "))
         smallY: int = int(input("Enter small Y: "))
 
+        return bigX, bigY, smallX, smallY
+
+    def play_turn(self) -> None:
+        print(f"{self.current_player}'s turn!")
+        (bigX, bigY, smallX, smallY) = self.get_turn_coordinates()
+
         if not self.board.play_turn(self.current_player, bigX, bigY, smallX, smallY):
-            print("Invalid move!")
+            print("Invalid move! Try again.")
             return
 
         if self.board.check_small_win(self.current_player, bigX, bigY):
-            print(f"{self.current_player} wins the small board!")
-            #self.board.big_board[bigY, bigX] = self.current_player
+            print(f"{self.current_player} wins small board ({bigX},{bigY})!")
 
         if self.board.check_big_win(self.current_player):
             print(f"{self.current_player} wins the game!")
             self.winner = self.current_player
 
-        self.switch_player()
+        if self.board.check_small_board_valid(smallX, smallY):
+            self.next_x = smallX
+            self.next_y = smallY
+            return
 
-    def print(self):
+        self.next_x = -1
+        self.next_y = -1
+
+    def print(self) -> None:
         self.board.print()
