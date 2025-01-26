5c953fa93e6fa9caeac3ec63a424b88b9f34a846
Viktor Pazhev <asusvikp@gmail.com>
2025-01-26 19:42:00 +0200

    Add test mode

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import argparse
 from src.game import Game
 
 def main():
-    game: Game = Game()
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(prog="MegaTicTacToe", description="Play a game of Mega Tic Tac Toe!")
+    parser.add_argument("--test", action="store_true", help="Run the game in test mode. This will allow you to play anywhere on the board and to take no turns.")
+
+    args: argparse.Namespace = parser.parse_args()
+
+    game: Game = Game(test_mode=args.test)
     game.print()
 
 if __name__ == "__main__":
--- src/game.py
+++ src/game.py
@@ -1,7 +1,8 @@
 from .board import Board
 
 class Game:
-    def __init__(self):
+    def __init__(self, *, test_mode: bool = False):
+        self.test_mode: bool = test_mode
         self.player1: str = "O"
         self.player2: str = "X"
 
@@ -16,15 +17,11 @@ class Game:
     def play(self) -> None:
         while not self.board.is_full() and not self.winner:
             self.print()
-            self.play_turn()
-            self.switch_player()
+            if self.play_turn() and not self.test_mode:
+                self.switch_player()
 
         self.print()
-
-        if not self.winner:
-            print("It's a tie!")
-        else:
-            print(f"{self.winner} wins!")
+        print(f"{self.winner} wins!" if self.winner else "It's a tie!")
 
     def switch_player(self) -> None:
         if self.current_player == self.player1:
@@ -47,13 +44,13 @@ class Game:
 
         return bigX, bigY, smallX, smallY
 
-    def play_turn(self) -> None:
+    def play_turn(self) -> bool:
         print(f"{self.current_player}'s turn!")
         (bigX, bigY, smallX, smallY) = self.get_turn_coordinates()
 
         if not self.board.play_turn(self.current_player, bigX, bigY, smallX, smallY):
             print("Invalid move! Try again.")
-            return
+            return False
 
         if self.board.check_small_win(self.current_player, bigX, bigY):
             print(f"{self.current_player} wins small board ({bigX},{bigY})!")
@@ -62,13 +59,14 @@ class Game:
             print(f"{self.current_player} wins the game!")
             self.winner = self.current_player
 
-        if self.board.check_small_board_valid(smallX, smallY):
+        if not self.test_mode and self.board.check_small_board_valid(smallX, smallY):
             self.next_x = smallX
             self.next_y = smallY
-            return
+            return True
 
         self.next_x = -1
         self.next_y = -1
+        return True
 
     def print(self) -> None:
         self.board.print()
