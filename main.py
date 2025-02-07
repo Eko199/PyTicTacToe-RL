5c93bbd57b03c382266f22c7f330c5d57bee8697
Viktor Pazhev <asusvikp@gmail.com>
2025-02-07 16:20:25 +0200

    Add training algorithm

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     parser: argparse.ArgumentParser = argparse.ArgumentParser(prog="MegaTicTacToe", description="Play a game of Mega Tic Tac Toe!")
     parser.add_argument("--test", 
                         action="store_true", 
-                        help="Run the game in test mode. This will allow you to play anywhere on the board and to take no turns.")
+                        help="Run the game in test mode. This will allow you to play anywhere on the board and not to switch players.")
 
     args: argparse.Namespace = parser.parse_args()
     asyncio.run(main_menu(args))
--- src/agent/training.py
+++ src/agent/training.py
@@ -1,5 +1,28 @@
-import gymnasium as gym
-from typing import Any
+import os.path
+from stable_baselines3 import DQN
+from gymnasium.wrappers import FlattenObservation
+from .tictactoe_env import TicTacToeEnv
+from ..players.random_player import RandomPlayer
+from ..players.ai_player import AIPlayer
 
 MODELS_PATH = "src\\agent\\models"
-env: gym.Env[Any, Any] = gym.make('CartPole-v0')
+
+def train_model(name: str, steps: int, is_o: bool):
+    env = FlattenObservation(TicTacToeEnv([RandomPlayer()], train_x=False)) \
+        if is_o \
+        else FlattenObservation(TicTacToeEnv([RandomPlayer(), AIPlayer("o_trainer1")], train_x=True))
+
+    # Train the model (3M+ timesteps recommended)
+    model = DQN(
+        "MlpPolicy", env, verbose=1,
+        learning_rate=3e-4,
+        exploration_fraction=0.1,
+        exploration_final_eps=0.01,
+        buffer_size=100000,
+        batch_size=64,
+        train_freq=4,
+        target_update_interval=1000
+    )
+
+    model.learn(total_timesteps=steps)
+    model.save(os.path.join(MODELS_PATH, name))
